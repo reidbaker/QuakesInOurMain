d4e6ccf50839ce547eb2725ae1788894cb2c41f8
Reid Baker <hamiltonbaker@yahoo.com>
2012-10-31 00:29:06 -0400

    record done, date is giving me a negitive number for some reason

--- src/edu/gatech/earthquakes/components/Importer.java
+++ src/edu/gatech/earthquakes/components/Importer.java
@@ -5,14 +5,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
+import edu.gatech.earthquakes.model.DataRow;
 import edu.gatech.earthquakes.model.DataSet;
 
 public class Importer {
     private final static String dataLocation = "../data/";
     private final static String fileName = "Catalog.txt";
 
+    private final static String DATE = "Date";
+    private final static String RECORD = "Record";
     public static DataSet importData(){
         try {
             BufferedReader reader = new BufferedReader(new FileReader(new File(dataLocation + fileName)));
@@ -28,16 +36,33 @@ public class Importer {
         return null;
     }
 
-    private static void readQuake(BufferedReader reader) throws IOException{
-        Map<String, Object> = new M
-        String yearRecord = reader.readLine();
-        createDate(yearRecord.split("\\s")[0]);
+    private static DataRow readQuake(BufferedReader reader) throws IOException{
+        Map<String, Object> curQuake = new HashMap<String, Object>();
 
-        System.out.println(reader.readLine());
+        String[] yearRecord = reader.readLine().split("\\s");
+
+        String date = yearRecord[0];
+        curQuake.put(DATE, createDate(date));
+
+        String record = yearRecord[1];
+        curQuake.put(RECORD, record);
+
+        //System.out.println(reader.readLine());
+
+        return new DataRow(curQuake);
     }
 
-    private static String createDate(String yearMonthDay){
-        return yearMonthDay;
+    private static Date createDate(String yearMonthDay){
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(yearMonthDay);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(date.getYear());
+        System.out.println(yearMonthDay);
+        return date;
     }
 
 }
