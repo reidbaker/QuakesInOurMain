8585c11d9085bb90c8a9fed131bff55449504f8f
Elizabeth Johnson <ejohnson44@gatech.edu>
2012-11-01 03:21:51 -0400

    forgot these classes

---
+++ src/edu/gatech/earthquakes/vises/NominalBarGraph.java
@@ -0,0 +1,18 @@
+package edu.gatech.earthquakes.vises;
+
+import processing.core.PApplet;
+
+public class NominalBarGraph extends BarGraph
+{
+	public NominalBarGraph(int x, int y, int w, int h, String dataType) {
+		super(x, y, w, h, dataType);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void drawComponent(PApplet parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
