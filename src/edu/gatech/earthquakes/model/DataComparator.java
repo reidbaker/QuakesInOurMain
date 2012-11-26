6a9632d2dc5b1c46af24e470fa28d4d1e2ef3aaa
Taylor Wrobel <taylor.wrobel@gmail.com>
2012-11-26 16:50:23 -0500

    Added more sort fields to comparator, and added FIXME for Reid

--- src/edu/gatech/earthquakes/model/DataComparator.java
+++ src/edu/gatech/earthquakes/model/DataComparator.java
@@ -8,8 +8,9 @@ public class DataComparator implements Comparator<DataRow> {
 	// TODO: Taylor add Continental Locaiton, Dependency, Type, Depth
 	public enum CompareCategories {
 		DATE(false), DATE_REVERSE(true), MAGNITUDE(false), MAGNITUDE_REVERSE(
-				true);
-		
+				true), CONTINENT(false), DEPENDENCY(false), TYPE(false), DEPTH(
+				false), DEPTH_REVERSED(true);
+
 		private boolean reversed;
 
 		private CompareCategories(boolean reversed) {
@@ -55,6 +56,38 @@ public class DataComparator implements Comparator<DataRow> {
 			if (m0.compareTo(m1) != 0)
 				return (!categories[index].isReversed() ? m0.compareTo(m1) : m1
 						.compareTo(m0));
+			break;
+		case CONTINENT:
+			DataRow.Continent c0 = (DataRow.Continent) arg0.getVariables().get(
+					DataRow.CONTINENT);
+			DataRow.Continent c1 = (DataRow.Continent) arg1.getVariables().get(
+					DataRow.CONTINENT);
+			if (c0.compareTo(c1) != 0)
+				return c0.compareTo(c1);
+			break;
+		case DEPENDENCY:
+			DataRow.Dependency dependency0 = (DataRow.Dependency) arg0
+					.getVariables().get(DataRow.DEPENDENCY);
+			DataRow.Dependency dependency1 = (DataRow.Dependency) arg1
+					.getVariables().get(DataRow.DEPENDENCY);
+			if (dependency0.compareTo(dependency1) != 0)
+				return dependency0.compareTo(dependency1);
+			break;
+		case DEPTH:
+		case DEPTH_REVERSED:
+			Double depth0 = (double) arg0.getVariables().get(DataRow.DEPTH);
+			Double depth1 = (double) arg1.getVariables().get(DataRow.DEPTH);
+			if (depth0.compareTo(depth1) != 0)
+				return (!categories[index].isReversed() ? depth0
+						.compareTo(depth1) : depth1.compareTo(depth0));
+			break;
+		case TYPE:
+			DataRow.Type t0 = (DataRow.Type) arg0.getVariables().get(
+					DataRow.TYPE);
+			DataRow.Type t1 = (DataRow.Type) arg1.getVariables().get(
+					DataRow.TYPE);
+			if (t0.compareTo(t1) != 0)
+				return t0.compareTo(t1);
 			break;
 		}
 		return compareLeveled(arg0, arg1, index + 1);
