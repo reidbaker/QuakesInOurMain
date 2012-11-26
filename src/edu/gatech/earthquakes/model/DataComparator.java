56acd53d72eb47e9bbb2855ac2b8a758e1f0a947
Taylor Wrobel <taylor.wrobel@gmail.com>
2012-11-25 22:23:35 -0500

    Expanded DataComparator to take multiple categories for comparison

--- src/edu/gatech/earthquakes/model/DataComparator.java
+++ src/edu/gatech/earthquakes/model/DataComparator.java
@@ -3,37 +3,57 @@ package edu.gatech.earthquakes.model;
 import java.util.Comparator;
 import java.util.Date;
 
+public class DataComparator implements Comparator<DataRow> {
 
-public class DataComparator implements Comparator<DataRow>{
-	
 	public enum CompareCategories {
-		DATE,
-		MAGNITUDE,
-	}
-	
-	private boolean reversed;
-	private CompareCategories type;
-	
-	
-	public DataComparator(CompareCategories compareType, boolean reversed){
-		this.type = compareType;
-		this.reversed = reversed;
+		DATE(true), DATE_REVERSE(false), MAGNITUDE(true), MAGNITUDE_REVERSE(
+				false);
+
+		private boolean reversed;
+
+		private CompareCategories(boolean reversed) {
+			this.reversed = reversed;
+		}
+
+		private boolean isReversed() {
+			return reversed;
+		}
 	}
 
+	private CompareCategories[] categories;
+
+	public DataComparator(CompareCategories... compareCategories) {
+		this.categories = compareCategories;
+	}
 
 	@Override
 	public int compare(DataRow arg0, DataRow arg1) {
-		switch(type){
-		case DATE:
-			Date d0 = (Date)arg0.getVariables().get(DataRow.DATE);
-			Date d1 = (Date)arg1.getVariables().get(DataRow.DATE);
-			return (!reversed?d0.compareTo(d1):d1.compareTo(d0));
-		case MAGNITUDE:
-			Double m0 = (double)arg0.getVariables().get(DataRow.MOMENT_MAGNITUDE);
-			Double m1 = (double)arg1.getVariables().get(DataRow.MOMENT_MAGNITUDE);
-			return (!reversed?m0.compareTo(m1):m1.compareTo(m0));
-		default:
+		return compareLeveled(arg0, arg1, 0);
+	}
+
+	public int compareLeveled(DataRow arg0, DataRow arg1, int index) {
+		if (index >= categories.length)
 			return 0;
+		switch (categories[index]) {
+		case DATE:
+		case DATE_REVERSE:
+			Date d0 = (Date) arg0.getVariables().get(DataRow.DATE);
+			Date d1 = (Date) arg1.getVariables().get(DataRow.DATE);
+			if (d0.compareTo(d1) != 0)
+				return (!categories[index].isReversed() ? d0.compareTo(d1) : d1
+						.compareTo(d0));
+			break;
+		case MAGNITUDE:
+		case MAGNITUDE_REVERSE:
+			Double m0 = (double) arg0.getVariables().get(
+					DataRow.MOMENT_MAGNITUDE);
+			Double m1 = (double) arg1.getVariables().get(
+					DataRow.MOMENT_MAGNITUDE);
+			if (m0.compareTo(m1) != 0)
+				return (!categories[index].isReversed() ? m0.compareTo(m1) : m1
+						.compareTo(m0));
+			break;
 		}
+		return compareLeveled(arg0, arg1, index + 1);
 	}
 }
