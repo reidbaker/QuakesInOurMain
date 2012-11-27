9debe11065650accf26e941dadb3564de292adab
Taylor Wrobel <taylor.wrobel@gmail.com>
2012-11-27 00:10:41 -0500

    Added basic resizing logic

---
+++ src/edu/gatech/earthquakes/interfaces/Resizable.java
@@ -0,0 +1,9 @@
+package edu.gatech.earthquakes.interfaces;
+
+import java.awt.Rectangle;
+
+public interface Resizable {
+
+	public void resizeTo(Rectangle bounds);
+	
+}
--- src/edu/gatech/earthquakes/vises/AbstractVisualization.java
+++ src/edu/gatech/earthquakes/vises/AbstractVisualization.java
@@ -1,10 +1,13 @@
 package edu.gatech.earthquakes.vises;
 
+import java.awt.Rectangle;
+
 import processing.core.PApplet;
 import edu.gatech.earthquakes.components.Theme;
 import edu.gatech.earthquakes.interfaces.Drawable;
+import edu.gatech.earthquakes.interfaces.Resizable;
 
-public abstract class AbstractVisualization implements Drawable{
+public abstract class AbstractVisualization implements Drawable, Resizable{
 
 	protected int x, y, w, h;
 	
@@ -17,5 +20,13 @@ public abstract class AbstractVisualization implements Drawable{
 		parent.stroke(Theme.getBaseUIColor());
 		parent.strokeCap(PApplet.ROUND);
 		parent.rect(x, y, w, h);
+	}
+	
+	@Override
+	public void resizeTo(Rectangle bounds) {
+		this.x = bounds.x;
+		this.y = bounds.y;
+		this.w = bounds.width;
+		this.h = bounds.height;
 	}
 }
