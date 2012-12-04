904e6753772c98de55038777089df7c0fa6788aa
Elizabeth Johnson <ejohnson44@gatech.edu>
2012-12-04 15:09:27 -0500

    cleaned up some of the circles code

--- src/edu/gatech/earthquakes/vises/NestedCirclePlot.java
+++ src/edu/gatech/earthquakes/vises/NestedCirclePlot.java
@@ -1,7 +1,6 @@
 package edu.gatech.earthquakes.vises;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,7 +15,6 @@ import edu.gatech.earthquakes.model.DataComparator;
 import edu.gatech.earthquakes.model.DataRow;
 import edu.gatech.earthquakes.model.DataSet;
 
-
 public class NestedCirclePlot extends Aggregate implements Filterable {
     // the thing that will be grouped by after location
     private String dataType;
@@ -24,36 +22,30 @@ public class NestedCirclePlot extends Aggregate implements Filterable {
     private Map<String, Set<TypeCount>> computedValues;
     private double maxVal = 0;
     private int offset = 15;
-    private boolean nominal = false;
     private TreeSet<TypeCount> totals;
+    private int numTotalQuakes;
 
     public NestedCirclePlot(int x, int y, int w, int h, DataSet displayData,
-            String dataType) {
+            String dataType, int numTotalQuakes) {
         super(x, y, w, h, displayData);
         this.dataType = dataType;
+        this.numTotalQuakes = numTotalQuakes;
 
         DataComparator.CompareCategories category = null;
         switch (dataType) {
-            case DataRow.MOMENT_MAGNITUDE:
-                category = DataComparator.CompareCategories.MAGNITUDE;
-                break;
             case DataRow.DEPENDENCY:
                 category = DataComparator.CompareCategories.DEPENDENCY;
-                nominal = true;
                 break;
             case DataRow.TYPE:
                 category = DataComparator.CompareCategories.TYPE;
-                nominal = true;
                 break;
         }
 
         dataComp = new DataComparator(
                 DataComparator.CompareCategories.CONTINENT, category);
+        
+        computeNominalData();
 
-        if (nominal)
-            computeNominalData();
-        else
-            computeNumericData();
     }
 
     public void drawComponent(PApplet parent) {
@@ -65,77 +57,78 @@ public class NestedCirclePlot extends Aggregate implements Filterable {
         boolean right = false;
         parent.noStroke();
 
-        parent.textSize(offset*3/4);
+        parent.textSize(offset * 3 / 4);
         parent.textAlign(PApplet.CENTER);
-        
+
         for (String country : computedValues.keySet()) {
 
+            //draw the colored square for the country and write the country name
             parent.fill(DataRow.getColorFor(country));
-          
             if (right) {
                 parent.rect(x + w / 2 + offset / 2,
                         drawY - maxCircleRadius * 2, maxCircleRadius * 2,
                         maxCircleRadius * 2);
                 parent.fill(0);
-                parent.text(country, x + w / 2 + offset / 2 + maxCircleRadius, drawY + offset*3/4);
-            } else{
+                parent.text(country, x + w / 2 + offset / 2 + maxCircleRadius,
+                        drawY + offset * 3 / 4);
+            } else {
                 parent.rect(x + w / 2 - offset / 2 - maxCircleRadius * 2, drawY
                         - maxCircleRadius * 2, maxCircleRadius * 2,
                         maxCircleRadius * 2);
                 parent.fill(0);
-                parent.text(country, x + w / 2 - offset / 2 - maxCircleRadius, drawY + offset*3/4);
+                parent.text(country, x + w / 2 - offset / 2 - maxCircleRadius,
+                        drawY + offset * 3 / 4);
             }
-            float firstRadius = 0;
-
+            
+            parent.strokeWeight(1);
+            //draw the circles for the country
             for (TypeCount t : computedValues.get(country)) {
 
-                if (firstRadius == 0)
-                    firstRadius = getCircleRadius(t.getCount());
+                parent.fill(Theme.changeSaturation(
+                        DataRow.getColorFor(t.getType()), .5f, true));
 
-                parent.fill(Theme.changeSaturation(DataRow.getColorFor(t.getType()),.5f,true));
-               // parent.fill(DataRow.getColorFor(t.getType()));
-               parent.strokeWeight(1);
                 parent.stroke(0xff555555);
 
                 float radius = getCircleRadius(t.getCount());
-                if (right) {
-                    parent.ellipse(x + w / 2 + offset / 2 + maxCircleRadius,
-                            drawY - radius, radius * 2, radius * 2);
-                } else
-
-                    parent.ellipse(x + w / 2 - offset / 2 - maxCircleRadius,
-                            drawY - radius, radius * 2, radius * 2);
+                if (right)
+                    parent.ellipse(x + w / 2 + offset / 2 + maxCircleRadius, drawY - radius, radius * 2, radius * 2);
+                else
+                    parent.ellipse(x + w / 2 - offset / 2 - maxCircleRadius, drawY - radius, radius * 2, radius * 2);
             }
 
             if (right)
                 drawY -= maxCircleRadius * 2 + offset;
-
             right = !right;
         }
+        //draw the outline of the percentage square
+        parent.noFill();
+        parent.rect(x + w / 2 + offset / 2, drawY - maxCircleRadius * 2, maxCircleRadius * 2, maxCircleRadius * 2);
         
-        parent.stroke(0);
-
+        parent.noStroke();
+        //draw the total percentages (upper right corner square)
         for (TypeCount t : totals) {
-            int color =  Theme.changeSaturation(DataRow.getColorFor(t.getType()),.5f,true);
-            parent.fill(Theme.rgba(color,180));
-            float heightPercent = 0;//getCircleRadius(t.getCount())/getCircleRadius(count);
-
-            parent.rect(x + w / 2 + offset / 2, drawY - heightPercent*maxCircleRadius*2, maxCircleRadius*2,heightPercent*maxCircleRadius*2);
-            drawY -= heightPercent*maxCircleRadius*2;
+            int color = Theme.changeSaturation(
+                    DataRow.getColorFor(t.getType()), .5f, true);
+            parent.fill(color);
+            float heightPercent = t.getCount() / (float) numTotalQuakes;
+            parent.rect(x + w / 2 + offset / 2, drawY - heightPercent
+                    * maxCircleRadius * 2, maxCircleRadius * 2, heightPercent
+                    * maxCircleRadius * 2);
+            drawY -= heightPercent * maxCircleRadius * 2;
         }
 
     }
-
-    /*
-     * All of th scaling is done with the formula of:
+    
+    /**
+     * Calculates the radius of the circle based on the number of earthquakes that
+     * circle represents
      * 
-     * f(x) = (b-a)(x-min)/(max-min) + a
-     * 
-     * where [min,max] maps to [a,b]
+     * @param count
+     * @return
      */
-    // [min,max] = [0,maxVal] -> [a,b] = [0,maxArea]
     private float getCircleRadius(double count) {
-        float maxDiameter = Math.min( (float) ((h - buffer * 2 - offset
+        float maxDiameter = Math.min(
+                (float) ((h - buffer * 2 - offset
                         * (Math.ceil(computedValues.size() / 2.0) - 1)) / (Math
                         .ceil(computedValues.size() / 2.0))),
                 (w - buffer * 2 - offset * 2) / 2);
@@ -157,21 +150,21 @@ public class NestedCirclePlot extends Aggregate implements Filterable {
 
     private void calculateTotals() {
         HashMap<String, Integer> counts = new HashMap<String, Integer>();
-        
+
         for (Set<TypeCount> countryData : computedValues.values()) {
             for (TypeCount t : countryData) {
                 System.out.println(t.getType() + " " + t.getCount());
                 if (counts.containsKey(t.getType())) {
-                    
+
                     int count = counts.get(t.getType());
                     counts.put(t.getType(), t.getCount() + count);
-                } 
-                else
+                } else
                     counts.put(t.getType(), t.getCount());
             }
         }
 
         totals = new TreeSet<TypeCount>();
+        
         for (String type : counts.keySet()) {
             totals.add(new TypeCount(type, counts.get(type)));
             System.out.println("Type :" + counts.get(type));
@@ -179,80 +172,49 @@ public class NestedCirclePlot extends Aggregate implements Filterable {
     }
 
     private void computeNominalData() {
-        //populate the computed values with the continents and the set 
-        computedValues = new TreeMap<String,Set<TypeCount>>();
-        for(DataRow.Continent c: DataRow.Continent.values()){
+        // populate the computed values with the continents and the set
+        computedValues = new TreeMap<String, Set<TypeCount>>();
+        for (DataRow.Continent c : DataRow.Continent.values()) {
             computedValues.put(c.toString(), new TreeSet<TypeCount>());
         }
-        
+
         ArrayList<DataRow> list = new ArrayList<DataRow>(displayData.getDatum());
         Collections.sort(list, dataComp);
-        
+
         String type = null;
         String continent = null;
-        if(list.size() >0){
+        if (list.size() > 0) {
             type = list.get(0).getValue(dataType).toString();
-            continent = list.get(0).getValue(DataRow.CONTINENT).toString(); 
+            continent = list.get(0).getValue(DataRow.CONTINENT).toString();
         }
-        
+
         int count = 0;
-        System.out.println("Num Quakes: " + list.size());
-        int numQuakesCounted = 0;
+
         for (DataRow quake : list) {
             // get continent and type out of the current quake
             String curContinent = quake.getValue(DataRow.CONTINENT).toString();
             String curType = quake.getValue(dataType).toString();
-            
-            //if we've come to data from a new continent
-            if(!curContinent.equals(continent)){
+
+            // if we've come to data from a new continent
+            if (!curContinent.equals(continent)) {
                 computedValues.get(continent).add(new TypeCount(type, count));
                 count = 1;
                 type = curType;
                 continent = curContinent;
-            }
-            else{
+            } else {
                 if (curType.equals(type)) {
                     count++;
-                } 
-                else {
+                } else {
                     computedValues.get(curContinent).add(new TypeCount(type, count));
                     count = 1;
                     type = curType;
-                    numQuakesCounted += count;
                 }
             }
         }
         computedValues.get(continent).add(new TypeCount(type, count));
-        numQuakesCounted += count;
-        
-        System.out.println("NumQuakesCounted: " + numQuakesCounted);
+
         calculateTotals();
         computeMaxVal();
-    }
-
-    private void computeNumericData() {
-        computedValues = new TreeMap<String, Set<TypeCount>>();
-        // totals = new TreeMap<String, Integer>();
-        /*
-         * ArrayList<DataRow> list = new
-         * ArrayList<DataRow>(displayData.getDatum()); Collections.sort(list,
-         * dataComp);
-         * 
-         * for (DataRow quake : list) { // if we already have data from this
-         * continent String continent =
-         * quake.getValue(DataRow.CONTINENT).toString(); String bucket =
-         * quake.getValue(dataType).toString();
-         * 
-         * if (computedValues.containsKey(continent)) { if
-         * (computedValues.get(continent).containsKey(bucket)) { int value =
-         * computedValues.get(continent).get(bucket);
-         * computedValues.get(continent).put(bucket, ++value); } else{
-         * computedValues.get(continent).put(bucket, 1); } } // if we don't
-         * already have this continent else { computedValues.put(continent, new
-         * HashMap<String, Integer>());
-         * computedValues.get(continent).put(bucket, 1); } } calculateTotals();
-         * computeMaxVal();
-         */
     }
 
     @Override
@@ -275,7 +237,7 @@ public class NestedCirclePlot extends Aggregate implements Filterable {
         }
 
         public int compareTo(TypeCount t) {
-            return t.count-count;
+            return t.count - count;
         }
 
         public String getType() {
